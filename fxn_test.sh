f30215b62241dc53dbfc56c638a8f29d7802bc8f
tacaro <tristan.caro@colorado.edu>
2019-10-03 18:59:54 -0600

    fixed syntax error in fxn_test.sh

--- fxn_test.sh
+++ fxn_test.sh
@@ -1,10 +1,6 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-run test_exit_code python plot_gtex.py --gene_reads GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz --sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --gene ACTA2 --group_type SMTS --output_file ACTA2.png
-assert_exit_code 0
-
-rm ACTA2.png
 
 run test_no_gr python plot_gtex.py --sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt --gene ACTA2 --group_type SMTS --output_file ACTA2.png
 assert_exit_code 2
